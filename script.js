eef9cd5901a61b91dc396c53efe00ee485db0c63
sharstellar <shstellarr@gmail.com>
2024-12-20 02:02:44 -0800

    previous design

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 function toggleMobileMenu() {
-    document.getElementById("menu").classList.toggle("active");
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('active');
 }
